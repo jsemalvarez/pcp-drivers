2c47f2847290a20820c0dc75f2a8d0a0e7dd39dc
jsemalvarez <jsemalvarez@gmail.com>
2025-08-28 18:03:01 -0300

    feat: add basic LoginPage.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,10 @@
+import { LoginPage } from '../features/auth/Loginpage'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-        <h1 className="text-3xl font-bold underline">    
-          Hello world!  
-        </h1>
-    </>
+    <LoginPage />
   )
 }
 
---
+++ src/features/auth/LoginPage.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+export const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log({ email, password });
+    // Aquí va la lógica de login
+  };
+
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gradient-to-b from-primary to-secondary">
+      <div className="w-full max-w-sm bg-white rounded-2xl shadow-lg py-12 px-6 mx-4">
+        <h1 className="text-2xl font-bold text-center text-primary mb-6">
+          Login Chofer
+        </h1>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label className="block text-sm font-medium text-secondary">
+              Email
+            </label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="tu@email.com"
+              required
+              className="w-full px-4 py-2 mt-1 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium text-secondary">
+              Contraseña
+            </label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="••••••••"
+              required
+              className="w-full px-4 py-2 mt-1 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full py-2 bg-primary text-white font-semibold rounded-xl shadow cursor-pointer"
+          >
+            Ingresar
+          </button>
+        </form>
+
+      </div>
+    </main>
+  );
+}
